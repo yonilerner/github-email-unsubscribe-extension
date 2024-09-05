b890e26b7681d8f32d421e7f8627dcbef84b0eca
Yoni Lerner <yoni.lerner@discordapp.com>
2024-09-05 00:06:48 -0400

    ctrl click open

--- content.js
+++ content.js
@@ -1,7 +1,15 @@
 function clickPRLink() {
     const prLink = document.querySelector('a[href^="https://github.com/notifications/unsubscribe-auth/"]');
     if (prLink) {
-      prLink.click();
+      const event = new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+        button: 0,
+        metaKey: true,
+        ctrlKey: true,
+      });
+      prLink.dispatchEvent(event);
     } else {
       alert('PR link not found!');
     }
